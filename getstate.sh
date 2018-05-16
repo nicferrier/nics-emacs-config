00ec7eea6ff457b9bc59f0537b862f1c9e7e917e
Nic Ferrier <nic@ferrier.me.uk>
2018-05-16 18:57:19 +0100

    handle the download directory

--- getstate.sh
+++ getstate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat .packages | while read name remote
+grep -v '^#' .packages | while read name remote
 do
     clone=$(echo $remote \
                    | sed -re 's|git@(.*):(.*)$|https://\1/\2|p' \
@@ -13,8 +13,9 @@ do
 
         download)
             download=$(echo $clone | sed -rne 's|(.*).git|\1|p')
-            curl -Ls ${download}/archive/master.zip -o ${name}.zip
-            unzip ${name}.zip
+            curl -Ls ${download}/archive/master.zip -o .${name}.zip
+            zipdir=$(unzip .${name}.zip | sed -rne 's|creating: (.*)/|\1|p')
+            mv $zipdir $name
             ;;
 
         clone)
